f2677a8a789408476001e678560cd242d9d197e2
HsoulT <alxanderr@gmail.com>
2015-02-25 18:59:00 -0400

    Removed unnecessary files

--- mindstorm_sens_light.m
+++
@@ -1,8 +0,0 @@
-h = COM_OpenNXT();
-COM_SetDefaultNXT(h);
-OpenLight(SENSOR_3, 'ACTIVE'); % have to specify mode, ACTIVE or INACTIVE
-light = GetLight(SENSOR_3)
-pause(4);
-CloseSensor(SENSOR_3);
-voltage = NXT_GetBatteryLevel()
-COM_CloseNXT(COM_GetDefaultNXT()); 
