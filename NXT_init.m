7a52f984c4ed8ffa6962582480837b601f0e9221
CCalnan <clark.a.calnan@gmail.com>
2015-02-27 15:52:22 -0400

    Changes to NXT_init

--- NXT_init.m
+++ NXT_init.m
@@ -1,3 +1,10 @@
-COM_CloseNXT all
+function [mA, mB, mAB, mC] = NXT_init
 h = COM_OpenNXT();
 COM_SetDefaultNXT(h);
+OpenUltrasonic(SENSOR_4);
+mA = NXTMotor('A', 'SpeedRegulation', false, 'SmoothStart', true);
+mB = NXTMotor('B', 'SpeedRegulation', false, 'SmoothStart', true);
+mAB = NXTMotor('AB', 'SpeedRegulation', false, 'SmoothStart', true);
+mC = NXTMotor('C', 'SpeedRegulation', false, 'SmoothStart', true);
+NXT_PlayTone(440,100);
+end
