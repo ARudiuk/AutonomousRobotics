c36bcf45a059bde39f00dbcc8eba5b192a7de320
HsoulT <alxanderr@gmail.com>
2015-03-04 11:44:40 -0400

    Initial PID work

--- turn.m
+++ turn.m
@@ -7,8 +7,21 @@ function [] = turn(lwheel, rwheel, power, phi)
     lwheel.SendToNXT();
     rwheel.SendToNXT();
     temp = 0;
+    last_error = 0;
+    tic;
+    total_error = 0;
+    last_time = toc;
     while(temp<phi)
         temp = lwheel.ReadFromNXT().Position;
+        error = (phi-temp);
+        total_error = total_error + error;
+        temp_time = toc;
+        power = error + (error-last_error)/(temp_time-last_time) + total_error;
+        if power>100
+            power = 100
+        end
+        last_time = temp_time;
+        last_error = error;
     end
     lwheel.Stop('brake');
     rwheel.Stop('brake');
